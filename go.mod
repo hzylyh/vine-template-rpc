363f68c26c6b9ed1a025e25cac8f716605b8c63a
haMonkey <hzylyh@hotmail.com>
2023-10-22 21:26:08 +0800

    chore:添加ent依赖

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module vine-template-rpc
 go 1.19
 
 require (
+	entgo.io/ent v0.12.4
 	github.com/go-kratos/kratos/v2 v2.7.0
 	github.com/google/wire v0.5.0
 	go.uber.org/automaxprocs v1.5.1
@@ -27,12 +28,12 @@ require (
 	go.opentelemetry.io/otel v1.16.0 // indirect
 	go.opentelemetry.io/otel/metric v1.16.0 // indirect
 	go.opentelemetry.io/otel/trace v1.16.0 // indirect
-	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.11.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
-	golang.org/x/tools v0.6.0 // indirect
+	golang.org/x/tools v0.8.1-0.20230428195545-5283a0178901 // indirect
 	google.golang.org/genproto v0.0.0-20230629202037-9506855d4529 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230629202037-9506855d4529 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/system/data/data.go
+++ internal/system/data/data.go
@@ -8,3 +8,64 @@
  */
 
 package data
+
+import (
+	"context"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+	"vine-template-rpc/internal/conf"
+	"vine-template-rpc/internal/system/data/ent"
+	"vine-template-rpc/internal/system/data/ent/migrate"
+)
+
+// ProviderSet is data providers.
+var ProviderSet = wire.NewSet(
+	NewData,
+	NewEntClient,
+	//NewRedisCmd,
+	//NewUserRepo,
+	//NewCardRepo,
+	//NewAddressRepo,
+)
+
+// Data .
+type Data struct {
+	db *ent.Client
+	//redisCli redis.Cmdable
+}
+
+// NewEntClient 创建ent客户端
+func NewEntClient(conf *conf.Data, logger log.Logger) *ent.Client {
+	log := log.NewHelper(log.With(logger, "module", "user-service/data/ent"))
+
+	client, err := ent.Open(
+		conf.Database.Driver,
+		conf.Database.Source,
+	)
+	if err != nil {
+		log.Fatalf("failed opening connection to db: %v", err)
+	}
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background(), migrate.WithForeignKeys(false)); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+	return client
+}
+
+// NewData .
+func NewData(
+	entClient *ent.Client,
+	//redisCmd redis.Cmdable,
+	logger log.Logger) (*Data, func(), error) {
+	log := log.NewHelper(log.With(logger, "module", "user-service/data"))
+
+	d := &Data{
+		db: entClient,
+		//redisCli: redisCmd,
+	}
+	return d, func() {
+		if err := d.db.Close(); err != nil {
+			log.Error(err)
+		}
+	}, nil
+}
